c925b25a5dffa08f897466cd18039fed7e7bd517
SEBonduel <sebastien050599@hotmail.fr>
2026-03-17 15:50:47 +0100

    Feature : add main menu and level complete improvements

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -9,27 +9,65 @@ public class LevelComplete : MonoBehaviour
     public TextMeshProUGUI levelText;
     public Button nextButton;
 
+    bool isShowing = false;
+    bool isLastLevel = false;
+
     void Start()
     {
         panel.SetActive(false);
-        nextButton.onClick.AddListener(LoadNextLevel);
+        nextButton.onClick.AddListener(OnNextPressed);
+    }
+
+    void Update()
+    {
+        if (isShowing && Input.GetKeyDown(KeyCode.Return))
+        {
+            OnNextPressed();
+        }
+        if (isShowing && Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartLevel();
+        }
     }
 
     public void Show()
     {
         int levelNumber = SceneManager.GetActiveScene().buildIndex + 1;
-        levelText.text = "Niveau " + levelNumber + " termine";
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        isLastLevel = nextSceneIndex >= SceneManager.sceneCountInBuildSettings;
+
+        if (isLastLevel)
+        {
+            levelText.text = "Bravo ! Tous les niveaux sont termines !";
+            nextButton.GetComponentInChildren<TextMeshProUGUI>().text = "Menu";
+        }
+        else
+        {
+            levelText.text = "Niveau " + levelNumber + " termine";
+        }
+
         panel.SetActive(true);
+        isShowing = true;
         Time.timeScale = 0f;
     }
 
-    void LoadNextLevel()
+    void RestartLevel()
     {
         Time.timeScale = 1f;
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+    void OnNextPressed()
+    {
+        Time.timeScale = 1f;
+
+        if (isLastLevel)
         {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
             SceneManager.LoadScene(nextSceneIndex);
         }
     }
